9e35894b893e2d4f469c8c990419ba214fa0730d
OlhaY <olgarshv@gmail.com>
2015-08-06 09:27:53 +0300

    practice 4 adding

--- Practice/src/Quiz6.java
+++ Practice/src/Quiz6.java
@@ -42,16 +42,20 @@ class Quiz6 {
     }
     public static void print(ArrayList<String> l) {
         // Тут дописать тело функции.
-        System.out.println(l);
+
+            System.out.println(l);
+
     }
     public static void print(String[] a) {
         // Тут дописать тело функции.
-        System.out.println(a);
+
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(a[i]);
+        }
     }
     // Тут дописать недостающие функции.
     public static boolean isListsEqual(ArrayList<String> l1, ArrayList<String> l2) {
         // Тут дописать тело функции.
-        boolean check = true;
         if (l1.size() == l2.size() && l1.equals(l2)) {
             return true;
         } else {
@@ -61,12 +65,21 @@ class Quiz6 {
 
     public static boolean isEqual(String[] a1, ArrayList<String> a2) {
         // Тут дописать тело функции.
-        if (a1.length  == a2.size() && a1.equals(a2)) {
+        /*if (a1.length  == a2.size() && a1.equals(a2)) {
             return true;
         }
         else {
             return false;
-        }
+        }*/
+
+        boolean tr=true;
+        for (int i = 0; i <= 4; i++) {
+            if (a1[i] != a2.get(i)) {
+                tr = false;
+                break;
+            }
+        }return tr;
     }
 }
+
 
