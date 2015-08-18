8509e550151ba98a7978c7c5914f86bdb4dd2f9c
OlhaY <olgarshv@gmail.com>
2015-08-18 15:38:09 +0300

    practice 4 adding

--- Practice/src/com/example/helloworld/XPathHomeWork.java
+++ Practice/src/com/example/helloworld/XPathHomeWork.java
@@ -3,6 +3,8 @@ package com.example.helloworld;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 /**
  * Created by OlhaY on 8/17/2015.
  */
@@ -19,11 +21,19 @@ public class XPathHomeWork {
         WebElement j = drv.findElementByXPath("//div/a[text() = 'Request access']");
         //расносильно записи  j = drv.findElement(By.xpath("//*[@name = 'q']"));
         j.click();
-
-
+        Practice8.slp(2);
+        WebElement EnterField = drv.findElementByXPath("//label");
+        System.out.println(EnterField.getText());
+        WebElement AlreadyLink = drv.findElementByXPath("//div/a[text() = 'Already have an invite code?']");
+        //расносильно записи  j = drv.findElement(By.xpath("//*[@name = 'q']"));
+        AlreadyLink.click();
         Practice8.slp(2);
 
-        drv.quit();
+        List<WebElement> res = drv.findElementsByXPath("//md-input-container/label");
+        for (WebElement w : res) {
+            System.out.println(w.getText());
+        }
+            drv.quit();
 
     }
 }
