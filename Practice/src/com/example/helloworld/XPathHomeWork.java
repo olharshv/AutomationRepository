755bb8989e9aa26fd414abe2d1f37203f35f74f2
OlhaY <olgarshv@gmail.com>
2015-08-18 19:20:58 +0300

    practice 4 adding

--- Practice/src/com/example/helloworld/XPathHomeWork.java
+++ Practice/src/com/example/helloworld/XPathHomeWork.java
@@ -28,12 +28,34 @@ public class XPathHomeWork {
         //расносильно записи  j = drv.findElement(By.xpath("//*[@name = 'q']"));
         AlreadyLink.click();
         Practice8.slp(2);
-
+//6. Напечатать в консоль заголовки всех полей кроме вьібора страньі.
         List<WebElement> res = drv.findElementsByXPath("//md-input-container/label");
         for (WebElement w : res) {
             System.out.println(w.getText());
+            //ng-pristine ng-valid md-input ng-touched class name
+
+            WebElement Country = drv.findElementByXPath("//md-input-container/label[text() = 'State/Province']");
+            if (Country.isDisplayed()) {
+                continue;
+            }
         }
-            drv.quit();
+        Practice8.slp(2);
+       // 7. Заполнить первьіе 3 поля.
+        WebElement firstField = drv.findElementByXPath("//md-input-container/input[@name = 'inviteCode']");
+        firstField.sendKeys("12345");
+        Practice8.slp(2);
+        WebElement secondField = drv.findElementByXPath("//md-input-container/input[@name = 'firstname']");
+        secondField.sendKeys("Olha");
+        Practice8.slp(2);
+        WebElement thirdField = drv.findElementByXPath("//md-input-container/input[@name = 'lastname']");
+        thirdField.sendKeys("Yershova");
+        Practice8.slp(2);
+        //8. Напечатать в консоль ошибку которая появилась возле первого поля.
+        WebElement error = drv.findElementByXPath("//md-input-container[1]//*[@class='ng-scope']");
+        System.out.println(error.getText());
+        Practice8.slp(2);
+        drv.quit();
+
 
     }
 }
