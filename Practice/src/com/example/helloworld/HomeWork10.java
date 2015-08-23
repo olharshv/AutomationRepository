7d1e18ad1f8eaf213fd22378f9f4e670cef56e04
OlhaY <olgarshv@gmail.com>
2015-08-23 19:10:53 +0300

    practice 4 adding

--- Practice/src/com/example/helloworld/HomeWork10.java
+++ Practice/src/com/example/helloworld/HomeWork10.java
@@ -1,6 +1,7 @@
 package com.example.helloworld;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,9 +14,27 @@ public class HomeWork10 {
         System.setProperty("webdriver.chrome.driver", "C:\\libJava\\chromedriver.exe");
         WebDriver drv = new ChromeDriver();
         drv.get("https://stage.tether.to");
-        findElement("//*[@name = 'site']", drv).sendKeys(" ");
+        System.out.println(drv.getTitle()); //в консоль выведется "Google"
+
+        findElement("//input[@name = 'username']", drv).sendKeys(" ");
+        findElement("//input[@name = 'password']", drv).sendKeys(" ");
+        System.out.println(findElement("//md-input-container[1]//*[@class='ng-scope']", drv).getText());
+        findElement("//input[@name = 'password']", drv).sendKeys(Keys.TAB);
+        System.out.println(findElement("//md-input-container[2]/div/*[@class = 'ng-scope']", drv).getText());
+
+        slpMs(3000);
+
         drv.quit();
 
+    }
+
+//sleep with milli seconds
+    public static void slpMs(long msec) {
+        try {
+            Thread.sleep(msec);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public static WebElement findElement(String targetXPath, WebDriver wdr) {
@@ -25,7 +44,7 @@ public class HomeWork10 {
             {
                 break;
             }
-            Practice8.slp(100);
+            slpMs(100);
         }
         return wdr.findElement(By.xpath(targetXPath));
     }
