74baf2e7d5d2a9f0b8a22060b706fbd82b53e1a5
OlhaY <olgarshv@gmail.com>
2015-09-08 20:49:05 +0300

    practice 4 adding

---
+++ Practice/src/com/example/helloworld/Practice14.java
@@ -0,0 +1,149 @@
+package com.example.helloworld;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by OlhaY on 9/8/2015.
+ */
+public class Practice14 {
+//убрать переменную drv из всех функций
+    //убираем у ф-й static, чтобы у них был доступ к переменной класса drv
+public static String url = /*"http://angel.net/~nic/passwd.current.html"*/ "http://oxogamestudio.com/passwd.current2.htm";
+
+    public static void main() {
+    }
+
+    @Test
+    public void test1() {
+        TestHelper.setMaster("master");
+        TestHelper.setSitename("google.com");
+        TestHelper.generate();
+        System.out.println(TestHelper.getPassword());
+        TestHelper.slpMs(5000);
+    }
+
+    @Test
+    public void test2() {
+        TestHelper.setMaster("hello");
+        TestHelper.setSitename("google.com");
+        TestHelper.generate();
+        System.out.println(TestHelper.getPassword());
+        TestHelper.slpMs(5000);
+    }
+    //
+    @Test
+    public void test3() {
+        TestHelper.setMaster("qwe123");
+        TestHelper.setSitename("gmail.com");
+        TestHelper.submitEnter();
+        Assert.assertEquals("mKJAakDvwbhi6@1a", TestHelper.getPassword());
+        TestHelper.slpMs(5000);
+    }
+
+    @Test
+    public void test4(){
+        TestHelper.setMaster("qwe123");
+        TestHelper.setSitename("gmail.com");
+        TestHelper.generate();
+        Assert.assertEquals("mKJAakDvwbhi6@1a", TestHelper.getPassword());
+        TestHelper.slpMs(5000);
+    }
+
+    @Test
+    public void test5(){
+        TestHelper.setMaster("");
+        TestHelper.setSitename("");
+        TestHelper.generate();
+        Assert.assertEquals("BaefBs8/Z/cm2@1a", TestHelper.getPassword());
+        TestHelper.slpMs(5000);
+    }
+
+    @Test
+    public void test6(){
+        TestHelper.setMaster("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        TestHelper.setSitename("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+        TestHelper.generate();
+        Assert.assertEquals("D3YvdfMKPtXGn@1a", TestHelper.getPassword());
+        TestHelper.slpMs(5000);
+    }
+
+    @Test
+    public void test7(){
+        TestHelper.setMaster("");
+        TestHelper.submitEnter();
+        TestHelper.generate();
+        Assert.assertEquals("BaefBs8/Z/cm2@1a", TestHelper.getPassword());
+        TestHelper.slpMs(5000);
+    }
+
+    @Test
+    public void test8(){
+        TestHelper.setMaster("333");
+        TestHelper.setSitename("111");
+        TestHelper.generate();
+        Assert.assertEquals("CD/iUTsNwzV4v@1a", TestHelper.getPassword());
+        TestHelper.slpMs(5000);
+    }
+
+    @Test
+    public void test9(){
+        TestHelper.setMaster("111");
+        TestHelper.setSitename("333");
+        TestHelper.generate();
+        Assert.assertEquals("A61tAOVfX4wCm@1a", TestHelper.getPassword());
+        TestHelper.slpMs(5000);
+    }
+
+    @Test
+    public void test10(){
+        TestHelper.setMaster("~!@#$%^&*()_+{}|\":?></\\");
+        TestHelper.setSitename("~!@#$%^&*()_+{}|\":?></\\");
+        TestHelper.generate();
+        Assert.assertEquals("mWxGz2VYwGgZK@1a", TestHelper.getPassword());
+        TestHelper.slpMs(5000);
+    }
+
+
+    @Test
+    public void test11(){
+        TestHelper.setMaster("qwe123");
+        TestHelper.setSitename("asd");
+        TestHelper.generate();
+        Assert.assertEquals("agdjoTB4cjI5E@1a", TestHelper.getPassword());
+        TestHelper.slpMs(5000);
+    }
+
+    @Test
+    public void test12(){
+        TestHelper.setMaster("qwe123");
+        TestHelper.setSitename("ASD");
+        TestHelper.generate();
+        Assert.assertEquals("FkfaCm2ddwvDg@1a", TestHelper.getPassword());
+        TestHelper.slpMs(5000);
+    }
+
+    @Test
+    public void test13(){
+        TestHelper.setMaster("qwe123");
+        TestHelper.setSitename("gmail.com");
+        TestHelper.generate();
+        Assert.assertEquals("qwe123", TestHelper.getMaster());
+        Assert.assertEquals("gmail.com", TestHelper.getSitename());
+        TestHelper.slpMs(5000);
+    }
+
+    @Before
+    public void funcBeforeEveryTest() {
+        TestHelper.setupUrl(Practice14.url);
+    }
+
+    //чтобы после каждого теста закрывался браузер. Иначе при фейле будет открыт браузер
+    // и следующие тесты - браузеры открыты, но не закрыты
+    @After
+    public void funcAfterEveryTest() {
+        TestHelper.quit();
+    }
+}
---
+++ Practice/src/com/example/helloworld/TestHelper.java
@@ -0,0 +1,90 @@
+package com.example.helloworld;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by OlhaY on 9/8/2015.
+ */
+public class TestHelper {
+
+
+    //сначала сделали переменную drv static в классе Practice14
+    // перенесли переменную drv из класса Practice14: правой кнопкой на переменной > refactor > move
+    //в Practice14 уже перед drv появился TestHelper
+    static WebDriver drv;
+
+    static void slpMs(long msec) {
+        try {
+            Thread.sleep(msec);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static WebElement findElement(String targetXPath) {
+        for (int i = 0; i < 600; i++)
+        {
+            if (drv.findElements(By.xpath(targetXPath)).size() > 0)
+            {
+                break;
+            }
+            slpMs(100);
+        }
+        return drv.findElement(By.xpath(targetXPath));
+    }
+
+    //функция  - заполнить первое поле
+    static void setMaster(String filledString){
+        findElement("//td/input[@name='master']").sendKeys(filledString);
+    }
+
+    //функция - заполнить второе поле
+    static void setSitename(String filledString){
+        findElement("//td/input[@name='site']").sendKeys(filledString);;
+    }
+
+    //функция - нажать кнопку Generate
+    static void generate(){
+        findElement("//td/input[@value = 'Generate']").click();
+    }
+
+    //функция  - достать из 3го
+    static String getPassword(){
+        return findElement("//td/input[@name= 'password']").getAttribute("value");
+    }
+
+    //Enter function
+    static void submitEnter(){
+        findElement("//td/input[@name= 'password']").sendKeys(Keys.ENTER);
+    }
+//вынесли в отдельную функцию, чтобы не делать каждый раз setProperties
+    static void setup(){
+        System.setProperty("webdriver.chrome.driver", "C:\\libJava\\chromedriver.exe");
+        TestHelper.drv = new ChromeDriver();
+    }
+
+    static void get(String url){
+        drv.get(url);
+    }
+
+    static void setupUrl(String abs){
+        setup();
+        get(abs);
+    }
+
+    static void quit (){
+        TestHelper.drv.quit();
+    }
+
+    public static String getMaster(){
+        return findElement("//td/input[@name='master']").getAttribute("value");
+    }
+
+    public static String getSitename(){
+        return findElement("//td/input[@name='site']").getAttribute("value");
+    }
+}
