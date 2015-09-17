bc12f00ac9b9a299e6dfcfc29ba041a5b0db7697
OlhaY <olgarshv@gmail.com>
2015-09-17 15:26:15 +0300

    practice 4 adding

--- Practice/src/com/example/helloworld/AngelPage.java
+++ Practice/src/com/example/helloworld/AngelPage.java
@@ -6,69 +6,77 @@ import org.openqa.selenium.Keys;
  * Created by OlhaY on 9/10/2015.
  */
 public class AngelPage {
+    private static String elementMaster = "//tr[1]/td/input";
+    private static String elementSitename = "//tr[2]/td/input";
+    private static String elementGenerateButton = "//tr[3]/td/input";
+    private static String elementGeneratedPassword = "//tr[4]/td/input";
+    private static String elementValueMaster = "//tr[1]/td";
+    private static String elementValueSitename = "//tr[2]/td";
+    private static String elementValueGeneratedPassword = "//tr[4]/td";
+
     //функция  - заполнить первое поле
     static void setMaster(String filledString){
-        TestHelper.findElement("//tr[1]/td/input").sendKeys(filledString);
+        TestHelper.findElement(elementMaster).sendKeys(filledString);
     }
 
     //функция - заполнить второе поле
     static void setSitename(String filledString){
-        TestHelper.findElement("//tr[2]/td/input").sendKeys(filledString);;
+        TestHelper.findElement(elementSitename).sendKeys(filledString);;
     }
 
     public static String getMaster(){
-        return TestHelper.findElement("//tr[1]/td/input").getAttribute("value");
+        return TestHelper.findElement(elementMaster).getAttribute("value");
     }
 
     public static String getSitename(){
-        return TestHelper.findElement("//tr[2]/td/input").getAttribute("value");
+        return TestHelper.findElement(elementSitename).getAttribute("value");
     }
 
     //функция - нажать кнопку Generate
     static void generate(){
-        TestHelper.findElement("//td/input[@value = 'Generate']").click();
+        TestHelper.findElement(elementGenerateButton).click();
     }
 
     //функция  - достать из 3го
     static String getPassword(){
-        return TestHelper.findElement("//tr[4]/td/input").getAttribute("value");
+        return TestHelper.findElement(elementGeneratedPassword).getAttribute("value");
     }
 
     //Enter function
     static void submitEnter(){
-        TestHelper.findElement("//tr[2]/td/input").sendKeys(Keys.ENTER);
+        TestHelper.findElement(elementSitename).sendKeys(Keys.ENTER);
     }
 
     static boolean MasterEnable(){
-        return TestHelper.findElement("//tr[1]/td/input").isEnabled();
+        return TestHelper.findElement(elementMaster).isEnabled();
     }
 
     static boolean PasswordEnable(){
-        return TestHelper.findElement("//tr[2]/td/input").isEnabled();
+        return TestHelper.findElement(elementSitename).isEnabled();
     }
 
     static boolean GeneratedPasswordEnable(){
-        return TestHelper.findElement("//tr[4]/td/input").isEnabled();
+        return TestHelper.findElement(elementGeneratedPassword).isEnabled();
     }
 
     static String getMasterFieldName(){
-        return TestHelper.findElement("//tr[1]/td").getText();
+        return TestHelper.findElement(elementValueMaster).getText();
     }
 
     static String getSiteFieldName(){
-        return TestHelper.findElement("//tr[2]/td").getText();
+        return TestHelper.findElement(elementValueSitename).getText();
     }
 
     static String getGenerateButtonName(){
-        return TestHelper.findElement("//tr[3]/td/input").getAttribute("value");
+        return TestHelper.findElement(elementGenerateButton).getAttribute("value");
     }
 
     static String getGeneratedPasswordName(){
-        return TestHelper.findElement("//tr[4]/td").getText();
+        return TestHelper.findElement(elementValueGeneratedPassword).getText();
     }
 
-    static void waitForGeneratedPassword() { TestHelper.waitForValue("//tr[4]/td/input");    }
+    static void waitForGeneratedPassword() { TestHelper.waitForValue(elementGeneratedPassword);    }
 
-    static void waitForGenerateButton() { TestHelper.waitForValue("//tr[3]/td/input");    }
+    static void waitForGenerateButton() { TestHelper.waitForValue(elementGenerateButton);    }
 
 }
